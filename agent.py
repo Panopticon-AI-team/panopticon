9bc7007a8443a4523c788d580f367e996f58e939
orgulous <lchenhw@gmail.com>
2023-09-19 00:01:44 -0400

    added state summarizer, action handler, and also way to visualize again. but the map isn't working right

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 """
 This is the agent, and it is being used. right now the action is just hardcoded for testing
 """
+from actionhandler import Action
 
 
 class Agent:
@@ -11,31 +12,11 @@ class Agent:
         time = s.time
         if time == 0:
             # take an arbitrary action to test
-            action = Action("JBER", "launch", "JBER1", "moveTo", "CN")
-            action1 = Action("JBER", "idle", "JBER1", "moveTo", "CN")
+            action = Action("JBER", "launch", "JBER1", "moveto", "CN")
+            action1 = Action("JBER", "idle", "JBER1", "moveto", "CN")
 
         actions = (action, action1)
         return actions
 
     def learn(self, s, a, reward, s_prime):
         self.qtable.update(s, a, reward, s_prime, self.alpha, self.gamma)
-
-
-# basically like a struct for actions
-class Action:
-    def __init__(self, base_chosen, base_action, unit_chosen, unit_action, unit_target):
-        self.base_chosen = base_chosen
-        self.base_action = base_action
-        self.unit_chosen = unit_chosen
-        self.unit_action = unit_action  # what the unit chosen will do
-        self.unit_target = unit_target  # the object of the unit's action
-
-    def __str__(self):
-        str = (
-            f"Base Chosen: {self.base_chosen}\n"
-            f"Base Action: {self.base_action}\n"
-            f"Unit Chosen: {self.unit_chosen}\n"
-            f"Unit Action: {self.unit_action}\n"
-            f"Unit Target: {self.unit_target}"
-        )
-        return str
--- environment.py
+++ environment.py
@@ -4,16 +4,25 @@ lists all the classes and stuff. this is an important file
 import json
 import math
 import copy
+from actionhandler import ActionHandler
+from statesummarizer import StateSummarizer
+import tkinter as tk
+from canvas import Canvas
 
 
 class Munition:
     def __init__(self, unit, munition_json):
         self.unit = unit  # where it belongs
         self.munition_id = munition_json["munition_id"]
+        self.munition_loc_x = unit.unit_loc_x
+        self.munition_loc_y = unit.unit_loc_y
         self.pk = munition_json["pk"]
         self.range = munition_json["range"]
         self.speed = munition_json["speed"]
         self.action = "idle"
+
+    def get_loc(self):
+        return self.munition_loc_x, self.munition_loc_y
 
 
 class Unit:
@@ -36,35 +45,8 @@ class Unit:
     def get_munitions(self):
         return self.munitions_ls
 
-    def compute_deltas(self, x1, y1, x2, y2, s):
-        # Vector from (x1, y1) to (x2, y2)
-        v = (x2 - x1, y2 - y1)
-
-        # Magnitude of V
-        mag_v = math.sqrt(v[0] ** 2 + v[1] ** 2)
-
-        # Handle the case when the two points are the same
-        if mag_v == 0:
-            return (0, 0)
-
-        # Vector U with magnitude s pointing in the same direction as V
-        dx, dy = (s * v[0] / mag_v, s * v[1] / mag_v)
-
-        return dx, dy
-
-    def handle_action(self, action):
-        self.action = action.unit_action
-        self.target = action.unit_target
-
-        if self.action == "moveTo":
-            x1, y1 = self.unit_loc_x, self.unit_loc_y
-            x2, y2 = self.target.get_loc()
-            dx, dy = self.compute_deltas(x1, y1, x2, y2, self.speed)
-
-            self.unit_loc_x = self.unit_loc_x + dx
-            self.unit_loc_y = self.unit_loc_y + dy
-        else:
-            print("some other action was selected, not implemented yet")
+    def get_loc(self):
+        return self.unit_loc_x, self.unit_loc_y
 
 
 class Base:
@@ -90,16 +72,6 @@ class Base:
     def get_base_name(self):
         return self.base_name
 
-    def handle_action(self, action):
-        chosen_unit = action.unit_chosen
-
-        for unit in self.units_ls:
-            if unit.unit_id == chosen_unit:
-                unit.handle_action(action)
-                return unit
-            else:
-                print("unit not found")
-
 
 class GameState:
     def __init__(self, config):
@@ -109,7 +81,9 @@ class GameState:
         self.units_ls = []
         self.munitions_ls = []
 
-        # initialize everything
+        # initialize everything by base, and then
+
+        # these each "unroll" the base into the lists
         for base_json in bases_json_ls:
             base = Base(base_json)
             self.bases_ls.append(base)
@@ -124,46 +98,14 @@ class GameState:
             for munition in munitions_ls:
                 self.munitions_ls.append(munition)
 
-    def print_status(self):
-        for base in self.bases_ls:
-            print(base.base_name)
-        for unit in self.units_ls:
-            print(unit.unit_id)
-        for munition in self.munitions_ls:
-            print(munition.munition_id)
-
-    def print_unit_coordinates(self):
-        for unit in self.units_ls:
-            print(f"The unit {unit.unit_id} is at {unit.unit_loc_x} {unit.unit_loc_y}")
-
     def get_bases(self):
         return self.bases_ls
 
-    def get_named_base(self, base_chosen_name):
-        print("the base chosen is " + base_chosen_name)
-        for base in self.bases_ls:
-            print(base.get_base_name())
-            # if it equals a name of the base in the list
-            if base.get_base_name() == base_chosen_name:
-                print("found the base")
-                return base
+    def get_units(self):
+        return self.units_ls
 
-                # print("base not found")
-
-    def handle_action(self, action):
-        # preprocessing for actions to get base/tgt objects, relies on other fun
-        print(action)
-        action.base_chosen = self.get_named_base(action.base_chosen)
-        action.unit_target = self.get_named_base(action.unit_target)
-
-        # go further into the action tree
-
-        action.base_chosen.handle_action(action)
-
-        """
-        unit_ls = self.get_units() # gets all units from the bases 
-        missile_ls = self.get_missiles() # gets all missiles
-        """
+    def get_munitions(self):
+        return self.munitions_ls
 
 
 class Environment:
@@ -177,7 +119,8 @@ class Environment:
             print("Error: The config file is not in proper JSON format.")
 
         self.game_state = GameState(config)
-        self.game_state.print_status()
+        ssumarizer = StateSummarizer(copy.deepcopy(self.game_state))
+        ssumarizer.print_unit_coordinates()
         self.time = 0
         print("hi")
 
@@ -190,14 +133,17 @@ class Environment:
         action, action1 = actions
 
         print("taking actions")
-        self.game_state.print_unit_coordinates()
+        ssumarizer = StateSummarizer(self.game_state)
 
-        self.game_state.handle_action(action)
-        self.game_state.print_unit_coordinates()
-        self.game_state.handle_action(copy.deepcopy(action1))
-        self.game_state.print_unit_coordinates()
-        self.game_state.handle_action(copy.deepcopy(action1))
-        self.game_state.print_unit_coordinates()
+        ahandler = ActionHandler(self.game_state)
+
+        ssumarizer.print_unit_coordinates()
+        ahandler.apply_action(action)
+        ssumarizer.print_unit_coordinates()
+        ahandler.apply_action(copy.deepcopy(action1))
+        ssumarizer.print_unit_coordinates()
+        ahandler.apply_action(copy.deepcopy(action1))
+        ssumarizer.print_unit_coordinates()
         print("action handled...?")
 
         self.time = self.time + 1
