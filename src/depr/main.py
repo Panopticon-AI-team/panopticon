66ecad88477d9988f71f02f6979ac2c17e0cd41c
Minh <minhhua12345@gmail.com>
2023-09-28 23:33:58 -0500

    re-organized file structure

---
+++ src/depr/main.py
@@ -0,0 +1,36 @@
+'''
+
+REVOLUTIONIZING THE ART OF WAR
+
+'''
+
+# Defining main function
+def main():
+
+    Munition(id=1, speed=10, , pk = .75, damage = 20):
+    
+    # initialize all bases, assets at bases, and weapons loadout
+  
+    '''
+    Train
+        need way to save and load weights eventually
+    execute x episodes
+
+        initialize game instance
+        Run episode while recording rewards
+        Game ends after all bases destroyed or timeout
+        
+        Rewards:
+            base killed: 50
+            asset killed: 10
+            All bases destroyed 100
+    '''
+
+    
+    # display output
+
+  
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
