772a36fd67e58bc6f1f253bc79c5edca1225888a
Minh <minhhua12345@gmail.com>
2024-01-14 00:25:26 -0600

    final reset

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
 import './styles/App.css';
-import GameMap from './mapComponents/map/GameMap';
 
 function App() {
   return (
     <div className="App">
-      <GameMap center={[0, 0]} zoom={12}></GameMap>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
