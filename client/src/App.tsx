eaa99a5edc5225b0040e01d3dffb3c2b3eb68184
Minh <minhhua12345@gmail.com>
2024-02-17 16:25:48 -0600

    add SCS scenario

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import {
-  get as getProjection,
+  get as getProjection, transform,
 } from 'ol/proj.js';
 
 import './styles/App.css';
@@ -12,6 +12,7 @@ import Scenario from './game/Scenario';
 import Game from './game/Game';
 import { DEFAULT_OL_PROJECTION_CODE } from "./utils/constants";
 import defaultScenarioJson from './scenarios/default_scenario.json';
+import SCSScenarioJson from './scenarios/SCS.json';
 
 export default function App() {
   const sideBlue = new Side(uuidv4(), 'BLUE');
@@ -22,12 +23,14 @@ export default function App() {
   const theGame = new Game(currentScenario);
   theGame.currentSideName = sideBlue.name;
   const projection = getProjection(DEFAULT_OL_PROJECTION_CODE);
+  const defaultScenarioZoom = [0, 0]
+  const SCSScenarioZoom = [130.78675023228317, 28.14934438063878]
 
-  theGame.loadScenario(JSON.stringify(defaultScenarioJson)); // loads default scenario for easier testing
+  theGame.loadScenario(JSON.stringify(SCSScenarioJson)); // loads default scenario for easier testing
 
   return (
     <div className="App">
-      <ScenarioMap center={[0, 0]} zoom={5} game={theGame} projection={projection}></ScenarioMap>   
+      <ScenarioMap center={transform(SCSScenarioZoom, 'EPSG:4326', DEFAULT_OL_PROJECTION_CODE)} zoom={5.5} game={theGame} projection={projection}></ScenarioMap>   
     </div>
   );
 }
