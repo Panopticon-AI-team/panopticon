c3ffda28738dddd03359bf0d787e81280ede8545
Minh Hua <42155133+duyminh1998@users.noreply.github.com>
2024-04-07 16:29:34 -0500

    42 aircraft launched from ships and airbases cannot rtb if the ship or airbase is deleted (#57)
    * add unit test helper file, change test unit ids
    
    * fix bug #42 and add test

---
+++ client/src/tests/Game.test.js
@@ -0,0 +1,40 @@
+import { getTestGame } from "./helpers";
+
+describe("aircraft rtb", () => {
+  test("aircraft stops returning to airbase if airbase is deleted (bug #42)", () => {
+    const testGame = getTestGame();
+    const testScenario = testGame.currentScenario;
+    const testAircraft = testScenario.getAircraft("5");
+    const testAirbase = testScenario.getAirbase("3");
+    expect(testAircraft.homeBaseId).toBe(testAirbase.id);
+    expect(testAircraft.rtb).toBe(false);
+    testGame.aircraftReturnToBase(testAircraft.id);
+    expect(testAircraft.rtb).toBe(true);
+    expect(testAircraft.route.length).toBe(1);
+    expect(testAircraft.route[0][0]).toBe(testAirbase.latitude);
+    expect(testAircraft.route[0][1]).toBe(testAirbase.longitude);
+    testGame.removeAirbase(testAirbase.id);
+    expect(testAircraft.rtb).toBe(false);
+    expect(testAircraft.homeBaseId).toBe("");
+    expect(testAircraft.route.length).toBe(0);
+  });
+
+  test("aircraft stops returning to ship if ship is deleted (bug #42)", () => {
+    const testGame = getTestGame();
+    const testScenario = testGame.currentScenario;
+    const testAircraft = testScenario.getAircraft("5");
+    const testShip = testScenario.getShip("6");
+    testAircraft.homeBaseId = testShip.id;
+    expect(testAircraft.homeBaseId).toBe(testShip.id);
+    expect(testAircraft.rtb).toBe(false);
+    testGame.aircraftReturnToBase(testAircraft.id);
+    expect(testAircraft.rtb).toBe(true);
+    expect(testAircraft.route.length).toBe(1);
+    expect(testAircraft.route[0][0]).toBe(testShip.latitude);
+    expect(testAircraft.route[0][1]).toBe(testShip.longitude);
+    testGame.removeShip(testShip.id);
+    expect(testAircraft.rtb).toBe(false);
+    expect(testAircraft.homeBaseId).toBe("");
+    expect(testAircraft.route.length).toBe(0);
+  });
+});
