e05799aad3747599ba9e903a0d3073b1e296692f
Luke Chen <lchenhw@gmail.com>
2024-10-05 21:27:33 -0400

    changed file path

--- gym/scripts/demo.py
+++ gym/scripts/demo.py
@@ -3,11 +3,11 @@ import blade
 from blade.Game import Game
 from blade.Scenario import Scenario
 
-game = Game(current_scenario = Scenario())
-with open("./gym/blade/scenarios/default_scenario.json", "r") as scenario_file:
+game = Game(current_scenario=Scenario())
+with open("../blade/scenarios/default_scenario.json", "r") as scenario_file:
     game.load_scenario(scenario_file.read())
 
-env = gymnasium.make('blade/BLADE-v0', game = game)
+env = gymnasium.make("blade/BLADE-v0", game=game)
 
 observation, info = env.reset()
 env.pretty_print(observation)
