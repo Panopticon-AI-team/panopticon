a6605bad6af1bcc611df6904a720e58160608b54
Minh <minhhua12345@gmail.com>
2025-03-27 16:18:17 -0500

    WIP aircraft example

--- client/src/gui/map/playback/PlaybackRecorder.ts
+++ client/src/gui/map/playback/PlaybackRecorder.ts
@@ -15,17 +15,17 @@ interface RecordingInfo {
 
 interface AircraftUpdate {
   id: string;
-  name: string;
-  className: string;
-  latitude: number;
-  longitude: number;
-  altitude: number;
-  heading: number;
-  speed: number;
-  currentFuel: number;
-  maxFuel: number;
-  fuelRate: number; // lbs/hr
-  range: number;
+  name?: string;
+  className?: string;
+  latitude?: number;
+  longitude?: number;
+  altitude?: number;
+  heading?: number;
+  speed?: number;
+  currentFuel?: number;
+  maxFuel?: number;
+  fuelRate?: number;
+  range?: number;
   route?: number[][];
   weapons?: Weapon[];
   rtb?: boolean;
@@ -75,6 +75,66 @@ class PlaybackRecorder {
       newReferencePoints: scenario.referencePoints,
     };
     this.recordedSteps.push(change);
+  }
+
+  getAircraftChanges(nextAircraft: Aircraft[]) {
+    const previousAircraft = this.previousStep?.aircraft ?? [];
+    const previousAircraftIds = previousAircraft.map((aircraft) => aircraft.id);
+    const nextAircraftIds = nextAircraft.map((aircraft) => aircraft.id);
+    // add new aircraft
+    const newAircraft = nextAircraft.filter(
+      (aircraft) => !previousAircraftIds.includes(aircraft.id)
+    );
+    // remove deleted aircraft
+    const deletedAircraftIds =
+      previousAircraft
+        .filter((aircraft) => !nextAircraftIds.includes(aircraft.id))
+        .map((aircraft) => aircraft.id) ?? [];
+    // update other aircraft
+    const updatedAircraft = nextAircraft.filter((aircraft) =>
+      previousAircraftIds.includes(aircraft.id)
+    );
+    const aircraftUpdates: AircraftUpdate[] = [];
+    updatedAircraft.forEach((currentAc) => {
+      const previousAc = previousAircraft.find((ac) => ac.id === currentAc.id);
+      if (!previousAc) {
+        return;
+      }
+      const update: AircraftUpdate = {
+        id: currentAc.id,
+      };
+      if (previousAc.name !== currentAc.name) update.name = currentAc.name;
+      if (previousAc.className !== currentAc.className)
+        update.className = currentAc.className;
+      if (previousAc.latitude !== currentAc.latitude)
+        update.latitude = currentAc.latitude;
+      if (previousAc.longitude !== currentAc.longitude)
+        update.longitude = currentAc.longitude;
+      if (previousAc.altitude !== currentAc.altitude)
+        update.altitude = currentAc.altitude;
+      if (previousAc.heading !== currentAc.heading)
+        update.heading = currentAc.heading;
+      if (previousAc.speed !== currentAc.speed) update.speed = currentAc.speed;
+      if (previousAc.currentFuel !== currentAc.currentFuel)
+        update.currentFuel = currentAc.currentFuel;
+      if (previousAc.maxFuel !== currentAc.maxFuel)
+        update.maxFuel = currentAc.maxFuel;
+      if (previousAc.fuelRate !== currentAc.fuelRate)
+        update.fuelRate = currentAc.fuelRate;
+      if (previousAc.range !== currentAc.range) update.range = currentAc.range;
+      if (previousAc.route !== currentAc.route) update.route = currentAc.route;
+      if (previousAc.weapons !== currentAc.weapons)
+        update.weapons = currentAc.weapons;
+      if (previousAc.rtb !== currentAc.rtb) update.rtb = currentAc.rtb;
+      if (previousAc.targetId !== currentAc.targetId)
+        update.targetId = currentAc.targetId;
+      if (Object.keys(update).length > 1) aircraftUpdates.push(update);
+    });
+    return {
+      newAircraft,
+      deletedAircraftIds,
+      aircraftUpdates,
+    };
   }
 
   getReferencePointChanges(nextReferencePoints: ReferencePoint[]) {
@@ -132,6 +192,8 @@ class PlaybackRecorder {
     if (!this.previousStep) {
       return;
     }
+    const { newAircraft, deletedAircraftIds, aircraftUpdates } =
+      this.getAircraftChanges(currentStep.aircraft);
     const {
       newReferencePoints,
       deletedReferencePointIds,
@@ -140,6 +202,10 @@ class PlaybackRecorder {
     const change: Change = {
       currentTime: currentStep.currentTime,
     };
+    if (newAircraft.length > 0) change.newAircraft = newAircraft;
+    if (deletedAircraftIds.length > 0)
+      change.deletedAircraftIds = deletedAircraftIds;
+    if (aircraftUpdates.length > 0) change.aircraftUpdates = aircraftUpdates;
     if (newReferencePoints.length > 0)
       change.newReferencePoints = newReferencePoints;
     if (deletedReferencePointIds.length > 0)
@@ -170,6 +236,23 @@ class PlaybackRecorder {
     document.body.appendChild(downloadAnchorNode); // required for firefox
     downloadAnchorNode.click();
     downloadAnchorNode.remove();
+
+    // output JSONL file instead of JSON which might facilitate streaming
+    // const jsonlDataStr = this.recordedSteps.map((step) => {
+    //   return JSON.stringify(step);
+    // });
+    // const jsonlData = jsonlDataStr.join("\n");
+    // const jsonlDataStrUrl =
+    //   "data:text/json;charset=utf-8," + encodeURIComponent(jsonlData);
+    // const downloadJsonlAnchorNode = document.createElement("a");
+    // downloadJsonlAnchorNode.setAttribute("href", jsonlDataStrUrl);
+    // downloadJsonlAnchorNode.setAttribute(
+    //   "download",
+    //   `${this.recordingInfo.name}.jsonl`
+    // );
+    // document.body.appendChild(downloadJsonlAnchorNode); // required for firefox
+    // downloadJsonlAnchorNode.click();
+    // downloadJsonlAnchorNode.remove();
   }
 }
 
